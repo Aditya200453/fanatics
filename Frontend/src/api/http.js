d48dad82be90d7b676489039aceca08e9ecf6464
Jash-IG <Pantham.Jashwanth@insightglobal.com>
2026-05-20 12:12:51 +0530

    Modified the http.js

--- Frontend/src/api/http.js
+++ Frontend/src/api/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://localhost:8077",
+  baseURL: "http://localhost:8076",
   headers: { "Content-Type": "application/json" },
 });
 
